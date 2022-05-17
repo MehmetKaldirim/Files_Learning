5589ec919469d3437cc92365fbd3662471b583b5
Mehmet <mathdenizi@gmail.com>
2022-05-17 14:22:33 +0200

    After deleting file with nio package

--- File-01-Create/src/main/java/DeleteFile.java
+++ File-01-Create/src/main/java/DeleteFile.java
@@ -1,16 +1,36 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class DeleteFile {
-    public static void main(String[] args)
-    {
-        File file
-                = new File("Annotation.txt").getAbsoluteFile();
+    public DeleteFile() {
+    }
 
-        if (file.delete()) {
-            System.out.println("File deleted successfully");
-        }
-        else {
-            System.out.println("Failed to delete the file");
+    public static void main(String[] args) {
+        DeleteFile d = new DeleteFile();
+        d.deleteWithNio();
+
+    }
+
+    public void deleteWithNio() {
+        {
+            try {
+                Files.deleteIfExists(
+                        Paths.get("C:\\Users\\math\\IdeaProjects\\Learning_Files\\javaprogram.txt"));
+
+            } catch (NoSuchFileException e) {
+                System.out.println(
+                        "No such file/directory exists");
+            } catch (DirectoryNotEmptyException e) {
+                System.out.println("Directory is not empty.");
+            } catch (IOException e) {
+                System.out.println("Invalid permissions.");
+            }
+
+            System.out.println("Deletion successful.");
         }
     }
 }
